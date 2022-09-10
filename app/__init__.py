44956d923bb13621ecd6a274eb60701c2a4a96b9
joshy joy <joshyjoy@joshys-MacBook-Air.local>
2022-09-10 21:05:10 +0530

    Authenication Added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
-
-
 from flask import Flask
 from pymongo import MongoClient
+from flask_httpauth import HTTPTokenAuth
 
 
 #initialize app
@@ -13,6 +12,11 @@ app.config.from_pyfile('config.py')
 
 #establishing connection to default mongo config "localhost:27017"
 mongo = MongoClient('mongodb://localhost:27017/')
+
+tokens = {"B1n0FlddHVnfBLeAkC": "admin"}
+
+#Http Authentication
+auth = HTTPTokenAuth(scheme='Bearer')
 
 #importing views
 from app import controller
--- app/controller.py
+++ app/controller.py
@@ -1,8 +1,14 @@
 from crypt import methods
 from flask import request, jsonify
-from app import app, mongo
+from app import app, mongo, auth, tokens
+
+@auth.verify_token
+def verify_token(token):
+    if token in tokens:
+        return tokens[token]
 
 @app.route("/createUser", methods=["POST"])
+@auth.login_required
 def create_user():
     userData = request.json
     result = mongo.flask.Users.insert_one(userData)
